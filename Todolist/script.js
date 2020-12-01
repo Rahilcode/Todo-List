93875b85682ee32463b4399826fb77f42fc4aced
Rahilcode <raheelkhatttal@gmail.com>
2020-12-01 19:06:41 +0530

    Added some buttons

--- Todolist/script.js
+++ Todolist/script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.querySelector('#btn').disabled = true;
+    document.querySelector('.btn').disabled = true;
 
     document.querySelector('#task').onkeyup = () => {
         if(document.querySelector('#task').value.length > 0){
-            document.querySelector('#btn').disabled = false;
+            document.querySelector('.btn').disabled = false;
         } else {
-            document.querySelector('#btn').disabled = true;
+            document.querySelector('.btn').disabled = true;
         }
     }
 
@@ -15,10 +15,17 @@ document.addEventListener('DOMContentLoaded', function() {
     
     const li = document.createElement('li');
     li.innerHTML = task;
+    const check = document.createElement('button');
+    const bin = document.createElement('button');
+    check.innerHTML = '<i>ch</i>';
+    bin.innerHTML = '<i>del</i>';
+
     document.querySelector('#tasks').append(li);
+    document.querySelector('#tasks').append(check);
+    document.querySelector('#tasks').append(bin);
 
     document.querySelector('#task').value = '';
-    document.querySelector('#btn').disabled = true;
+    document.querySelector('.btn').disabled = true;
 
 
     return false;
